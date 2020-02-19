d6cdd145b59d6687bb8fc7b80c7ae9ad17654fc4
Carlos De La Roca <carlosdlroca80@gmail.com>
2020-02-18 23:54:59 -0800

    Add webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+var path = require("path");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    mode: "development",
+    entry: path.join(__dirname, "src/index.js"),
+    output: {
+        filename: "[name].js",
+        path: path.resolve(__dirname, "dist")
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ["babel-loader"]
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif|webm|mp4)$/,
+                use: {
+                    loader: "file-loader",
+                    options: {
+                        name: "[name].[hash].[ext]",
+                        outputPath: "imgs"
+                    }
+                }
+            }
+        ]
+    },
+    resolve: {
+        // Allows us to do absolute imports from "src"
+        modules: [path.join(__dirname, "src"), "node_modules"],
+        extensions: ["*", ".js"]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "Development Mode",
+            template: path.join(__dirname, "public/template.html"),
+            favicon: path.join(__dirname, "public/icon.svg")
+        })
+    ]
+};
---
+++ webpack.production.js
@@ -0,0 +1,69 @@
+var path = require("path"),
+    TerserPlugin = require("terser-webpack-plugin"),
+    HtmlWebpackPlugin = require("html-webpack-plugin"),
+    CleanWebpackPlugin = require("clean-webpack-plugin"),
+    MiniCssExtractPlugin = require("mini-css-extract-plugin"),
+    OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+
+module.exports = {
+    mode: "production",
+    entry: {
+        main: path.join(__dirname, "src/index.js")
+    },
+    output: {
+        filename: "main-[contentHash].bundle.js",
+        output: path.resolve(__dirname, "build"),
+        publicPath: "/"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ["babel-loader"]
+            },
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader"]
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif|webm|mp4)$/,
+                use: {
+                    loader: "file-loader",
+                    options: {
+                        name: "[name].[hash].[ext]",
+                        outputPath: "imgs"
+                    }
+                }
+            }
+        ]
+    },
+    optimization: {
+        minimizer: [
+            new OptimizeCssAssetsPlugin(),
+            new TerserPlugin(),
+            new HtmlWebpackPlugin({
+                title: "Production Mode",
+                templates: path.join(__dirname, "public/template.html"),
+                favicon: path.join(__dirname, "public/icon.svg"),
+                minify: {
+                    removeAttributeQuotes: true,
+                    collapseWhitespace: true,
+                    removeComments: true,
+                    removeEmptyAttributes=true,
+                    removeTagWhitespace=true
+                } 
+            })
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '[name].[contentHash].css'
+        }),
+        new CleanWebpackPlugin()
+    ],
+    resolve: {
+        modules: [path.join(__dirname, "src"), "node_modules"],
+        extensions: ["*", ".js", ".css"]
+    }
+};
