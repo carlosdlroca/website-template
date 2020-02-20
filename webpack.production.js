ad4dfb7025d8830664cd8309e67dda76fb9068f2
Carlos De La Roca <carlosdlroca80@gmail.com>
2020-02-19 19:51:47 -0800

    Remove publicPath option from output

--- webpack.production.js
+++ webpack.production.js
@@ -7,13 +7,10 @@ var path = require("path"),
 
 module.exports = {
     mode: "production",
-    entry: {
-        main: path.join(__dirname, "src/index.js")
-    },
+    entry: path.join(__dirname, "src/index.js"),
     output: {
         filename: "main-[contentHash].bundle.js",
-        path: path.resolve(__dirname, "build"),
-        publicPath: "/"
+        path: path.resolve(__dirname, "build")
     },
     module: {
         rules: [
